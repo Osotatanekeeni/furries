4fef31f18652f7d8dcc8fb09f453e4c08ba80933
Osotatanekeeni <tatanekari52@gmail.com>
2025-02-08 22:01:32 +0200

    Removed unused code

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -7,21 +7,14 @@ function HeroSection() {
         <Navigation />
         <div className='flex h-full  items-center justify-between text-white'>
             {/* Text Section */}
-        <div className='flex w-full lg:w-1/2 flex-col gap-4 px-16 py-4 font-medium'>
+        <div className='flex w-full flex-col gap-4 px-16 py-4 font-medium lg:w-1/2'>
             <h1 className='text-4xl lg:text-6xl' style={{ fontFamily: "Playfair Display"}}>TREAT YOUR FURRY FRIENDS TO THE BEST</h1>
-            <p className='lg:whitespace-nowrap text-lg font-light' style={{ fontFamily: "Urbanist"}}>Discover Toys treats and more for your beloved pets</p>
+            <p className='text-lg font-light lg:whitespace-nowrap' style={{ fontFamily: "Urbanist"}}>Discover Toys treats and more for your beloved pets</p>
             <button className='mt-8 w-2/6 rounded-3xl bg-furriesOrange p-3 font-light' style={{ fontFamily: "Urbanist"}}>Shop Now</button>
         </div>
 
         {/* Image Section */}
-        {/* <div className='w-1/2 h-full size-full object-cover' style={{ backgroundImage: `url(${HeroImage})` }}> */}
-        <img src={HeroImage} alt='Hero Image' className='hidden lg:block absolute right-0 top-2 z-0 size-full w-3/4 object-cover' />
-        {/* <div className='w-1/2 h-full bg-cover bg-bottom-right' style={{ backgroundImage: `url(${HeroImage})`, backgroundPosition: 'bottom right' }}> */}
-        {/* </div> */}
-        {/* </div> */}
-        {/* <div className='border'>
-        <img className='w-screen h-screen' src={HeroImage}/>
-        </div> */}
+        <img src={HeroImage} alt='Hero Image' className='absolute right-0 top-2 z-0 hidden size-full w-3/4 object-cover lg:block' />
         </div>
         
 
